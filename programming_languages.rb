35fe5681106bb8b890c6cb263b070d867d4a84c9
Jeffrey Hechler <jeffrey.hechler@gmail.com>
2016-12-06 17:29:30 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  prog_lang = []
+  languages.each do |style, language|
+    language.each do |l, typ|
+      typ.each do |t, type|
+        if new_hash[l] == nil
+        new_hash[l] = {type: type, style: [style]}
+      #new_hash[language] = {type: type, style: style}
+        else
+          style_array = new_hash[l][:style]
+          style_array.push(style)
+        end
+      end
+    end
+  end
+  binding.pry
+  new_hash
 end
